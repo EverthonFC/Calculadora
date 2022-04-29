70d880841b05b900b9d52844399b6b0a049c0a3b
Everton <everthonfc@gmail.com>
2022-04-29 17:18:26 -0300

    ajuste das operações

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -36,11 +36,100 @@ class CalcController{
         this._operation.pop()
     }
 
-    addOperation(value) {
 
+
+    getLastOperation(){
+       return this._operation[this._operation.length-1]
+    }
+
+
+
+    setLastOperation(value){
+        this._operation[this._operation.length-1] = value
+    }
+
+
+
+    isOperator(value){
+        return (["+", "-", "*", "%", "/"].indexOf(value) > -1)
+      
+    }
+
+
+    pushOperation(value){
         this._operation.push(value)
 
-        console.log(this._operation); 
+        if(this._operation.length>3){
+
+            this.calc()
+               
+        }
+    }
+
+    calc(){
+        let last = this._operation.pop()
+        let result = eval(this._operation.join(""))
+        this._operation = [result, last]
+
+        this.setLastNumberToDisplay()
+
+    }
+
+
+    setLastNumberToDisplay(){
+
+        let lastNumber
+        for (let i = this._operation.length-1 ; i >= 0; i--) {
+            if (!this.isOperator(this._operation[i])){
+                lastNumber = this._operation[i]
+                break
+            }
+            
+        }
+
+        this.displayCalc = lastNumber
+
+    }
+
+
+    addOperation(value) {
+
+        
+
+        if (isNaN(this.getLastOperation())){
+
+            if ( this.isOperator(value)){
+
+                this.setLastOperation(value)
+
+            }else if (isNaN(value)){
+                
+                
+            }else{
+                this.pushOperation(value)
+
+                this.setLastNumberToDisplay()
+            }
+
+        } else{
+
+            if(this.isOperator(value)){
+                this.pushOperation(value)
+
+
+            }else{
+                let newValue = this.getLastOperation().toString() + value.toString()
+                this.setLastOperation(parseInt(newValue))
+
+                this.setLastNumberToDisplay()
+            }
+
+          
+        }
+
+        
+
+        
     }
 
     setError(){
@@ -62,26 +151,29 @@ class CalcController{
                 break
 
             case "porcento" :
-                
+                this.addOperation("%")
                 break 
 
             case "divisao" :
-                
+                this.addOperation("/")
                 break 
 
             case "multiplicacao" :
-                
+                this.addOperation("*")
                 break 
 
             case "subtracao" :
-                
+                this.addOperation("-")
                 break 
 
             case "soma" :
-                
+                this.addOperation("+")
                 break     
             case "igual" :
                 
+                break 
+            case "ponto" :
+                this.addOperation(".")
                 break 
 
             case "0":
